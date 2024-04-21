ae623f9e9bc444be57e602dd7f0c6bbdb3ce10f4
Martin <1191731324@qq.com>
2024-04-21 15:24:52 +0100

    fix bug

--- file-reader.h
+++ file-reader.h
@@ -1,8 +1,8 @@
 #ifndef LAPLACE_SOLVER_FILE_READER_H
 #define LAPLACE_SOLVER_FILE_READER_H
 
-void read_dims(char *filename);
-void read_array(char *filename, int numOfValues);
+int read_dims(char *filename);
+double* read_array(char *filename, int numOfValues);
 void write_to_output_file(char *filename, double *output, int numOfValues);
 
 #endif //LAPLACE_SOLVER_FILE_READER_H

d4c8d6dd2aab6f024d8de9c61a71acc1e1129c28
Martin <1191731324@qq.com>
2024-04-26 14:35:43 +0100

    modify shell script, test all kinds of threads

--- get_report.sh
+++ get_report.sh
@@ -15,8 +15,19 @@
 
 
 echo '---Running ./heat-omp-gcc---'
+export OMP_NUM_THREADS=1
 time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
-time ./heat-omp-icc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_icc_256_1024_4096.dat
-time mpirun -np 4 ./heat-complete-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_mpicc_256_1024_4096.dat
-time mpirun -np 4 ./heat-complete-icc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_mpiicc_256_1024_4096.dat
+export OMP_NUM_THREADS=2
+time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
+export OMP_NUM_THREADS=4
+time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
+export OMP_NUM_THREADS=8
+time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
+export OMP_NUM_THREADS=16
+time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
+export OMP_NUM_THREADS=32
+time ./heat-omp-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_gcc_256_1024_4096.dat
+#time ./heat-omp-icc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_icc_256_1024_4096.dat
+#time mpirun -np 4 ./heat-complete-gcc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_mpicc_256_1024_4096.dat
+#time mpirun -np 4 ./heat-complete-icc 256 4096 ./COMP328_CA_DATA_FILES/input_1024.dat ./COMP328_CA_DATA_FILES/outputDemo_mpiicc_256_1024_4096.dat
 echo '---Ending ./heat-omp-gcc----'

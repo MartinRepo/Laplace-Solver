ae3ed5b48f9929f3e0af31c1ff4c417625650fde
Martin <1191731324@qq.com>
2024-04-27 14:31:53 +0100

    optimise

--- heat.c
+++ heat.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
-#include <sys/time.h>
 
 double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemps) {
     double *results = (double *)malloc(numTemps * sizeof(double));
-    double ***t = (double ***)malloc(2 * sizeof(double **));
 
-    int pointx = floor((N - 1) * 0.5);
-    int pointy = floor((N - 1) * 0.5);
-
-    for (int k = 0; k < 2; k++) {
-        t[k] = (double **)malloc(N * sizeof(double *));
-        for (int i = 0; i < N; i++) {
-            t[k][i] = (double *)malloc(N * sizeof(double));
-        }
-    }
-    struct timeval startTime, endTime;
-    long seconds, useconds;
-    double total_time;
     for (int temp = 0; temp < numTemps; temp++) {
-        gettimeofday(&startTime, NULL);
+        double ***t = (double ***)malloc(2 * sizeof(double **));
+
+        int pointx = floor((N - 1) * 0.5);
+        int pointy = floor((N - 1) * 0.5);
+
+        for (int k = 0; k < 2; k++) {
+            t[k] = (double **)malloc(N * sizeof(double *));
+            for (int i = 0; i < N; i++) {
+                t[k][i] = (double *)malloc(N * sizeof(double));
+            }
+        }
+
         for (int k = 0; k < 2; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -43,7 +40,7 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
             #pragma omp parallel for default(none) shared(t, curr, next, N) collapse(2)
             for (int i = 1; i < N-1; i++) {
                 for (int j = 1; j < N-1; j++) {
-                    t[next][i][j] = (t[curr][i - 1][j] + t[curr][i + 1][j] + t[curr][i][j - 1] + t[curr][i][j + 1]) / 4.0;
+                    t[next][i][j] = (t[curr][i - 1][j] + t[curr][i][j - 1] + t[curr][i + 1][j] + t[curr][i][j + 1]) / 4.0;
                 }
             }
             int temp_idx = curr;
@@ -51,21 +48,15 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
             next = temp_idx;
         }
         results[temp] = t[curr][pointx][pointy];
-        gettimeofday(&endTime, NULL);
-        seconds  = endTime.tv_sec  - startTime.tv_sec;
-        useconds = endTime.tv_usec - startTime.tv_usec;
-        total_time = seconds + useconds/1000000.0;
-
-        printf("Loop took %f seconds to execute.\n", total_time);
-    }
-
-    for (int k = 0; k < 2; k++) {
-        for (int i = 0; i < N; i++) {
-            free(t[k][i]);
+        for (int k = 0; k < 2; k++) {
+            for (int i = 0; i < N; i++) {
+                free(t[k][i]);
+            }
+            free(t[k]);
         }
-        free(t[k]);
+        free(t);
+        printf("this is temp %d", temp);
     }
-    free(t);
 
     return results;
 }

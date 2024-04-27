587ed476686cfa71b7aa3bf87b3f51e6bc8d9bd4
Martin <1191731324@qq.com>
2024-04-27 15:29:24 +0100

    optimise

--- heat.c
+++ heat.c
@@ -6,19 +6,22 @@
 double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemps) {
     double *results = (double *)malloc(numTemps * sizeof(double));
 
-    for (int temp = 0; temp < numTemps; temp++) {
-        double ***t = (double ***)malloc(2 * sizeof(double **));
-
-        int pointx = floor((N - 1) * 0.5);
-        int pointy = floor((N - 1) * 0.5);
-
-        for (int k = 0; k < 2; k++) {
-            t[k] = (double **)malloc(N * sizeof(double *));
-            for (int i = 0; i < N; i++) {
-                t[k][i] = (double *)malloc(N * sizeof(double));
-            }
+    double ***t = (double ***)malloc(2 * sizeof(double **));
+    for (int k = 0; k < 2; k++) {
+        t[k] = (double **)malloc(N * sizeof(double *));
+        for (int i = 0; i < N; i++) {
+            t[k][i] = (double *)malloc(N * sizeof(double));
         }
+    }
 
+    int pointx = floor((N - 1) * 0.5);
+    int pointy = floor((N - 1) * 0.5);
+
+    int start = floor((N-1) * 0.3);
+    int end = ceil((N-1) * 0.7);
+
+    for (int temp = 0; temp < numTemps; temp++) {
+        #pragma omp parallel for default(none) shared(t, N) collapse(3)
         for (int k = 0; k < 2; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -26,8 +29,6 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
                 }
             }
         }
-        int start = floor((N-1) * 0.3);
-        int end = ceil((N-1) * 0.7);
         for (int k = 0; k < 2; k++) {
             for (int j = start; j <= end; j++) {
                 t[k][j][N-1] = radTemps[temp];
@@ -37,7 +38,7 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
         int next = 1;
 
         for (int iter = 0; iter < maxIter; iter++) {
-            #pragma omp parallel for default(none) shared(t, curr, next, N) collapse(2)
+            #pragma omp parallel for default(none) shared(t, curr, next, N, maxIter) collapse(2)
             for (int i = 1; i < N-1; i++) {
                 for (int j = 1; j < N-1; j++) {
                     t[next][i][j] = (t[curr][i - 1][j] + t[curr][i][j - 1] + t[curr][i + 1][j] + t[curr][i][j + 1]) / 4.0;
@@ -48,15 +49,14 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
             next = temp_idx;
         }
         results[temp] = t[curr][pointx][pointy];
-        for (int k = 0; k < 2; k++) {
-            for (int i = 0; i < N; i++) {
-                free(t[k][i]);
-            }
-            free(t[k]);
-        }
-        free(t);
-        printf("this is temp %d", temp);
     }
 
+    for (int k = 0; k < 2; k++) {
+        for (int i = 0; i < N; i++) {
+            free(t[k][i]);
+        }
+        free(t[k]);
+    }
+    free(t);
     return results;
 }

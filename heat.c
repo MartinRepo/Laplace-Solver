8bd7f3c91f415c82de2e93ede81181a2b808390a
Martin <1191731324@qq.com>
2024-04-27 13:55:21 +0100

    optimise logic and add timer

--- heat.c
+++ heat.c
@@ -2,24 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
-
-void update_temperatures(double ***t, int curr, int next, int N) {
-#pragma omp parallel for default(none) shared(t, curr, next, N) collapse(2)
-    for (int i = 1; i < N-1; i++) {
-        for (int j = 1; j < N-1; j++) {
-            t[next][i][j] = (t[curr][i - 1][j] + t[curr][i + 1][j] + t[curr][i][j - 1] + t[curr][i][j + 1]) / 4.0;
-        }
-    }
-}
-
-void set_radiator(double **t1, double **t2, int N, double temp) {
-    int start = floor((N-1) * 0.3);
-    int end = ceil((N-1) * 0.7);
-    for (int j = start; j <= end; j++) {
-        t1[j][N-1] = temp;
-        t2[j][N-1] = temp;
-    }
-}
+#include <sys/time.h>
 
 double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemps) {
     double *results = (double *)malloc(numTemps * sizeof(double));
@@ -34,7 +17,11 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
             t[k][i] = (double *)malloc(N * sizeof(double));
         }
     }
+    struct timeval startTime, endTime;
+    long seconds, useconds;
+    double total_time;
     for (int temp = 0; temp < numTemps; temp++) {
+        gettimeofday(&startTime, NULL);
         for (int k = 0; k < 2; k++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
@@ -42,18 +29,34 @@ double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemp
                 }
             }
         }
-        set_radiator(t[0], t[1], N, radTemps[temp]); // Assume the initial radiator setup is done in t[0]
+        int start = floor((N-1) * 0.3);
+        int end = ceil((N-1) * 0.7);
+        for (int k = 0; k < 2; k++) {
+            for (int j = start; j <= end; j++) {
+                t[k][j][N-1] = radTemps[temp];
+            }
+        }
         int curr = 0;
         int next = 1;
 
         for (int iter = 0; iter < maxIter; iter++) {
-            update_temperatures(t, curr, next, N);
-            // Swap indices for next iteration
+            #pragma omp parallel for default(none) shared(t, curr, next, N) collapse(2)
+            for (int i = 1; i < N-1; i++) {
+                for (int j = 1; j < N-1; j++) {
+                    t[next][i][j] = (t[curr][i - 1][j] + t[curr][i + 1][j] + t[curr][i][j - 1] + t[curr][i][j + 1]) / 4.0;
+                }
+            }
             int temp_idx = curr;
             curr = next;
             next = temp_idx;
         }
         results[temp] = t[curr][pointx][pointy];
+        gettimeofday(&endTime, NULL);
+        seconds  = endTime.tv_sec  - startTime.tv_sec;
+        useconds = endTime.tv_usec - startTime.tv_usec;
+        total_time = seconds + useconds/1000000.0;
+
+        printf("Loop took %f seconds to execute.\n", total_time);
     }
 
     for (int k = 0; k < 2; k++) {
--- heat.h
+++ heat.h
@@ -1,8 +1,6 @@
 #ifndef LAPLACE_SOLVER_HEAT_H
 #define LAPLACE_SOLVER_HEAT_H
 
-void update_temperatures(double ***t, int curr, int next, int N);
-void set_radiator(double **t, int N, double temp);
 double *get_final_temperatures(int N, int maxIter, double *radTemps, int numTemps);
 
 #endif //LAPLACE_SOLVER_HEAT_H
